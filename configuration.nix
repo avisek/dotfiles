88834158d8a40a9942160f40c9419ee424204248
Avisek Das <avisekdas555@gmail.com>
2025-06-06 02:46:39 +0530

    feat: move to flake based configuration and add home manager

--- configuration.nix
+++ configuration.nix
@@ -111,7 +111,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     neovim
-    git
     tree
     bat
     gedit
@@ -168,4 +167,7 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
+
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 }
