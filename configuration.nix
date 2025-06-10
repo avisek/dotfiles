b9f07c0b50a2e8a5de4f95c2b699f97c86a4d549
Avisek Das <avisekdas555@gmail.com>
2025-06-11 02:36:54 +0530

    refactor: format using alejandra formatter

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,16 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -16,8 +18,8 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
@@ -159,7 +161,10 @@
   users.users.avisek = {
     isNormalUser = true;
     description = "Avisek Das";
-    extraGroups = [ "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "networkmanager"
+      "wheel" # Enable ‘sudo’ for the user.
+    ];
     packages = with pkgs; [
       # tree
     ];
@@ -237,8 +242,6 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
-
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 }
--- flake.nix
+++ flake.nix
@@ -7,31 +7,33 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    { nixpkgs, home-manager, ... }:
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.avisek = ./home.nix;
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.avisek = ./home.nix;
 
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to home.nix
-            }
-          ];
-        };
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
       };
-
-      # Enables `nix fmt` command
-      # https://nix.dev/manual/nix/2.25/command-ref/new-cli/nix3-fmt#examples
-      # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-      # https://github.com/NixOS/nixfmt?tab=readme-ov-file#nix-fmt-experimental
-      # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     };
+
+    # Enables `nix fmt` command
+    # https://nix.dev/manual/nix/2.25/command-ref/new-cli/nix3-fmt#examples
+    # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    # https://github.com/NixOS/nixfmt?tab=readme-ov-file#nix-fmt-experimental
+    # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+  };
 }
