f14b35d6f07a2fc9b9a0d7d7bdf9876c77b7c585
Avisek Das <avisekdas555@gmail.com>
2025-06-19 21:46:26 +0530

    feat: add "Bibata-Modern-Classic" cursor

--- home.nix
+++ home.nix
@@ -61,4 +61,32 @@
   #   enable = true;
   #   settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
   # };
+
+  # https://wiki.nixos.org/wiki/Hyprland#Theme_Support
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+    gtk.enable = true;
+    # x11.enable = true;
+  };
+
+  # gtk = {
+  #   enable = true;
+
+  #   theme = {
+  #     package = pkgs.flat-remix-gtk;
+  #     name = "Flat-Remix-GTK-Grey-Darkest";
+  #   };
+
+  #   iconTheme = {
+  #     package = pkgs.adwaita-icon-theme;
+  #     name = "Adwaita";
+  #   };
+
+  #   font = {
+  #     name = "Sans";
+  #     size = 11;
+  #   };
+  # };
 }
